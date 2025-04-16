8f0a582d92923a0bbfef812a7d70c09cb8b0b38b
julesnikiema <ajulesnikiema@gmailcom>
2025-04-16 10:17:48 +0000

    second principal layout

--- trouve-ma-maison/app/welcome/welcome.js
+++ trouve-ma-maison/app/welcome/welcome.js
@@ -1,20 +1,23 @@
 import{View,ImageBackground ,Text, StyleSheet, FlatList} from "react-native";
-import AntDesign from '@expo/vector-icons/AntDesign'; //ici j'ai importer l'icone recherche 
 import HouseList from "@/assets/datas/datas-appartements.json";
 import { createIconSetFromFontello } from "@expo/vector-icons";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const Page = () => {
 
     return (
+        // Affiche un conteneur principal, avec des styles définis dans styles.container.
     <View style={styles.container} >
 
+        {/* Ce bloc est l’en-tête : Affiche le mot “Découvrir”, Ensuite, une icône de loupe (la bibliothèque AntDesign est utilisée pour ça).
+        Le style de ce bloc est styles.FirstView, et celui du texte est styles.textofView. */}
+
         <View style={styles.FirstView} > 
-            <Text style={styles.textofView} >Decouvrir </Text>
-        
-            <AntDesign name="search1" size={30} color="blue" />
+            <Text style={styles.textofView} >Découvrir </Text> 
+            <AntDesign name="search1" size={35} color="black" />
         </View>
 
-        <FlatList 
+        <FlatList  // FlatList est une liste optimisée pour afficher beaucoup d’éléments rapidement.
             data ={HouseList}  
             horizontal
             contentContainerStyle={styles.contentContainer}
@@ -27,10 +30,35 @@ const Page = () => {
          resizeMode="cover"
          style={styles.ImageBackground}>
 
-        <View>
-            <Text>nouveaute</Text>
+        <View style={{flexDirection : "row"}} >
+        <View style={styles.tag} >
+        <Text style={styles.tagtext} >nouveauté</Text>
         </View>
-        </ImageBackground>
+        </View>
+         </ImageBackground>
+
+            {/* lieu de la maison + notation */}
+                <View style= {styles.sndView}>
+                    <Text style={styles.nameLO}>{item.location_city}</Text>
+                    <View style={styles.tagContainer} >
+                        <Text style={styles.sndViewText} >5.0</Text>
+                        <AntDesign name="star" size={18} color="yellow" />
+                    </View>
+                </View>
+
+            {/* le prix de la location + nombre de personnes qui ont visiter  */}
+
+                <View style= {styles.sndView1}>
+                    <Text style={styles.nameLO1}> $ {item.month_price} /mois</Text>
+                    <View style={styles.tagContainer} >
+                    <AntDesign name="user" size={18} color="yellow" />
+                        <Text style={styles.sndViewText}> 4 visiite </Text>
+                      
+                    </View>
+                </View>
+    
+    
+       
         </View>}  />
     </View>
     );
@@ -45,11 +73,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor:"#f5f5f5",
-        
+  
 
     },
     FirstView: {
-        backgroundColor:"red",
         flexDirection: "row",
         justifyContent: "space-between",
         marginHorizontal: 15,
@@ -59,7 +86,7 @@ const styles = StyleSheet.create({
 
     },
     textofView:{
-        fontSize : 30, fontWeight:"semibold", color:"blue" 
+        fontSize : 35, fontWeight:"bold", color:"black" 
 
     }, 
     contentContainer:{
@@ -69,11 +96,56 @@ const styles = StyleSheet.create({
         height: 200,
         borderRadius: 20,
         overflow: "hidden", // j'ai ajouter cette ligne pour pouvoi voir les bordures arrondis 
-
+        padding:15,
     },
     ImageBlock : {
         width: 300,
+        gap: 30,
+        marginLeft: 15,
 
-    }
+    },
+    tag:{
+        backgroundColor:"red",
+        borderRadius:10, 
+        padding:8,
+    },
+    tagtext:{
+        color:"white",
+
+    },
+    tagContainer : {
+        backgroundColor:"grey",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 5,
+        borderRadius: 5,
+        padding:5,
+        
+    },
+    sndView:{
+        flexDirection: "row",
+        alignItems:"center",
+        justifyContent: "space-between",
+        marginTop: 20,
+        
+    },
+    sndViewText: {
+        color:"white",
+        fontSize: 15,
+        fontWeight:"bold",
+    },
+    nameLO:{
+        fontSize: 20, fontWeight:"bold", color:"black",
+    },
+    nameLO1:{
+        fontSize: 20, fontWeight:"bold", color:"grey",
+    },
+    sndView1:{
+        flexDirection: "row",
+        alignItems:"center",
+        justifyContent: "space-between",
+        marginTop: 10,
+    },
+
 
 });
